64b3059e31a93e33aac2404c3127fecf78dc11f8
Lavern755 <Lavern755@126.com>
2018-09-20 22:54:37 +0800

    thymeleaf的使用

--- src/main/java/com/liwen/controller/HelloController.java
+++ src/main/java/com/liwen/controller/HelloController.java
@@ -1,5 +1,7 @@
 package com.liwen.controller;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -10,5 +12,11 @@ public class HelloController {
 	@RequestMapping("/hello")
 	public String hello() {
 		return "hello springboot";
+	}
+	
+	@RequestMapping("/success")
+	public String success(Map<String,Object> map) {
+		map.put("hello","Jim");
+		return "success";
 	}
 }
